baffd0620b142ebe93ea6d4e0783980dc318fba0
PAMCH <pamic7124@naver.com>
2023-02-03 20:52:22 +0900

    텍스트를 음성으로 변환하기 main3-1 수정

--- 텍스트를 음성으로 변환하기/main3-1.py
+++ 텍스트를 음성으로 변환하기/main3-1.py
@@ -1,6 +1,7 @@
-from gtts import gTTS
+from gtts import gTTS # gtts 라이브러리에서 gTTS 함수만 가져옴
 
 text = "안녕하세요. 파이썬과 40개의 작품들 입니다."
 
-tts = gTTS(text=text, lang='ko')
-tts.save(r"텍스트를 음성으로 변환하기\hi.mp3")
+tts = gTTS(text=text, lang='ko') # text의 문자열을 한글로 변환하여 할당
+tts.save(r"텍스트를 음성으로 변환하기\hi.mp3") # 해당 폴더에 hi.mp3로 저장
+#리눅스의 경우 tts.save(r"텍스트를 음성으로 변환하기/hi.mp3")
