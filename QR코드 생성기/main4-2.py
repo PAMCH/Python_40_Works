3fc8a51e649a31ada3748c73d9290faefa61ca3c
PAMCH <pamic7124@naver.com>
2023-02-04 23:05:58 +0900

    QR코드 생성기 main4-2 개선

--- QR코드 생성기/main4-2.py
+++ QR코드 생성기/main4-2.py
@@ -1,10 +1,10 @@
 import qrcode
 
-file_path = r'QR코드 생성기\qr코드모음.txt'
+file_path = r'QR코드 생성기\qr코드모음.txt' # qr코드모음 텍스트 파일을 할당
 
-with open(file_path, 'rt', encoding='UTF-8') as f :
-    read_lines = f.readlines()
+with open(file_path, 'rt', encoding='UTF-8') as f : # qr코드모음 텍스트 파일을 읽음
+    read_lines = f.readlines() # 텍스트 파일 값을 리스트 값으로 할당
 
     for line in read_lines :
-        line = line.strip()
-        print(line)
+        line = line.strip() # 문자열 별 개행문자 삭제
+        print(line) # 문자열 출력
