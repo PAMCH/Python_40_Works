dc924e16c2dc90f8a1cb219102b0a8624c9a1657
PAMCH <pamic7124@naver.com>
2023-02-08 21:03:19 +0900

    멀티쓰레드 프로그램 최종본 완성

---
+++ 멀티쓰레드 프로그램/main8-3.py
@@ -0,0 +1,13 @@
+import threading # 멀티쓰레드 프로그래밍을 위한 모듈
+
+def sum(name, value) :
+    for i in range(0, value) :
+        print(f"{name} : {i}")
+
+t1 = threading.Thread(target=sum, args=('1번 쓰레드', 10))
+t2 = threading.Thread(target=sum, args=('2번 쓰레드', 10))
+
+t1.start()
+t2.start()
+
+print("Main Thread")
