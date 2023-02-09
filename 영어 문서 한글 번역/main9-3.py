e9265bf20e46362fd4d8956ca07867ea3664ab9c
PAMCH <pamic7124@naver.com>
2023-02-09 21:01:05 +0900

    영어 문서 한글 번역 main9-3 개선

--- 영어 문서 한글 번역/main9-3.py
+++ 영어 문서 한글 번역/main9-3.py
@@ -3,11 +3,11 @@ import googletrans
 
 translator = googletrans.Translator()
 
-read_file_path = r"영어파일.txt"
+read_file_path = r"영어파일.txt" # 예제 파일 가져옴
 
-with open(read_file_path , 'r') as f :
+with open(read_file_path , 'r') as f : # 예제 파일을 한줄씩 읽어서 할당함
     readLines = f.readline()
 
-for lines in readLines :
+for lines in readLines : # 한줄 식 읽어와서 한글로 번역하고 출력
     res = translator.translate(lines, dest='ko')
     print(res.text)
