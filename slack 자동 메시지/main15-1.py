91b78c5231cc1d5c199a8a3e02d92ffc0a3a028f
PAMCH <pamic7124@naver.com>
2023-02-15 21:05:08 +0900

    slack 자동 메시지 main15-1 완성

---
+++ slack 자동 메시지/main15-1.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+slack_webhook_url = "https://hooks.slack.com/services/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"
+
+def sendSlackWebhook(strText) :
+    headers ={
+        "Content-type": "application/json"
+    }
+
+    data = {
+        "text" : strText
+    }
+
+    res = requests.post(slack_webhook_url, headers=headers, data=json.dumps(data))
+
+    if res.status_code == 200 :
+        return "ok"
+    else :
+        return "error"
+
+print(sendSlackWebhook("안녕하세요, 파이썬에서 보내는 메시지 입니다."))
